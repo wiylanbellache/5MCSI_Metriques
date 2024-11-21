8a57bd799cb5761e5f49d602a8d66ac255e9cbe7
wiylanbellache <87870503+wiylanbellache@users.noreply.github.com>
2024-11-21 15:59:21 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,8 +4,6 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-import requests
-
                                                                                                                                        
 app = Flask(__name__)
 
