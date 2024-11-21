1c7cbb61c4bbc5ff6ef078fe84f629e4ff5d54b4
wiylanbellache <87870503+wiylanbellache@users.noreply.github.com>
2024-11-21 15:14:56 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+
                                                                                                                                        
 @app.route('/')
 def hello_world():
